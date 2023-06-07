0e3b449365840c63ce66dec41f63f3588d00a4cf
garcimj2012 <132397931+garcimj2012@users.noreply.github.com>
2023-06-07 18:55:52 -0500

    config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxyldBgdRNwOfQ8pOQN3w_tAL6oaUe4VOFpCJ6sSg9Q85CIliUhjbA1otlESguLByRqTw/exec";
